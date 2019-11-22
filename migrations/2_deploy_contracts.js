5bbedaecfc3c77af205bd8b3c3e8ce8d6ece3e0e
masaun <masanoriuno@gmail.com>
2019-11-22 14:28:42 +0100

    Commentout modifier of onlyOwner in requestResult method

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+const HoneycombBetPool = artifacts.require("HoneycombBetPool");
+const LinkTokenInterface = artifacts.require("LinkTokenInterface");
+
+const linkTokenAddress = "0x20fE562d797A42Dcb3399062AE9546cd06f63280";
+const oracle = "0x4a3fbbb385b5efeb4bc84a25aaadcd644bd09721";
+const jobId = web3.utils.toHex("d02b14632b6141ec90bb8b2b9b937848");
+const paymentAmount = web3.utils.toWei("0.1");
+
+module.exports = async function (deployer) {
+    await deployer.deploy(HoneycombBetPool, linkTokenAddress, oracle, jobId, paymentAmount);
+    const honeycombBetPool = await HoneycombBetPool.deployed();
+
+    const linkToken = await LinkTokenInterface.at(linkTokenAddress);
+    await linkToken.transfer(honeycombBetPool.address, paymentAmount);
+};
