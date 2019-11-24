c745cb5cffbefaa813f510c93f3f1c52c76d092c
masaun <masanoriuno@gmail.com>
2019-11-24 20:47:20 +0100

    Add DisasterArea struct

--- contracts/storage/DrObjects.sol
+++ contracts/storage/DrObjects.sol
@@ -5,12 +5,19 @@ contract DrObjects {
 
 	// Beneficiaries of Insurance
 	struct Beneficiary {
-		address addr;
-		uint fundAmount;   // Total Fund Amount
-		string ipAddress;  // IP adress
+		address addr;      // Wallet address for funding and withdrawing 
+		uint fundAmount;   // Total fund amount of insurance
+		string ipAddress;  // IP adress of beneficiary
 		bool isDisaster;   // Whether beneficiary live around areas of disaster or not
 	}
 	
+
+    // Whether DisasterArea or not
+    struct DisasterArea {
+        string ipAddress;  // IP adress of area
+        bool isDisaster;   // Whether areas is happenn disaster or not
+    }
+    
 
 
     struct ExampleObject {
